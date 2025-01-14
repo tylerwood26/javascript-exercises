c1bfb1467fd8180f8347f1fd96ae533f0e6fa2ff
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-14 14:39:35 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringToArray = string.split("");
+    let reverseArray = stringToArray.reverse();
+    let reverseString = reverseArray.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
