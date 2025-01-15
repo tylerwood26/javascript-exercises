44c11bf5d75955ed4a1b7ed97d84e5e1f53dbb39
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-15 12:47:38 -0500

    Improve if statements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,7 @@
 const sumAll = function(numOne, numTwo) {
     if (numOne > numTwo) [numOne, numTwo] = [numTwo, numOne];
     if (numOne < 0 || numTwo < 0) return "ERROR";
-    if (Number.isInteger(numOne) === false || 
-    Number.isInteger(numTwo) === false) return "ERROR";
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
     let sum = 0;
     for (let i = numOne; i <= numTwo; i++) {
         sum += i;
