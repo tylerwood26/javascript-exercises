f5ff635d3d6af998678eb93b9f23d85f0ed694fc
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-15 12:45:05 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (numOne > numTwo) [numOne, numTwo] = [numTwo, numOne];
+    if (numOne < 0 || numTwo < 0) return "ERROR";
+    if (Number.isInteger(numOne) === false || 
+    Number.isInteger(numTwo) === false) return "ERROR";
+    let sum = 0;
+    for (let i = numOne; i <= numTwo; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
