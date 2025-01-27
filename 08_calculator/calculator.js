a9d6fbadcdd5590854489d8336721823108af415
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-27 11:12:48 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, currentValue) => {
+    return total + currentValue;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentValue) => {
+    return total * currentValue;
+  });
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let sum = num1;
+  for (let i = 1; i < num2; i++) {
+    sum *= num1;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let numArr = [];
+  for (let i = num; i > 0; i--) {
+    numArr.push(i);
+  };
+  return numArr.reduce((total, currentValue) => {
+    return total * currentValue;
+  });
 };
 
 // Do not edit below this line
